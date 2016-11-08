c44f674409ba256c3267d79b7488ad103cf50c93
Keith Clark <geauxlsutigers24@gmail.com>
2016-11-08 15:17:03 +0000

    created storefront and viewpages/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'by_category' => storefront#items_by_category
+
+  get 'storefront/items_by_category'
+
   resources :products
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'storefront#all_items'
 end
