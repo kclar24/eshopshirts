5b78d7f2173b630b48d3c2172a094e7fb39c7812
Keith Clark <geauxlsutigers24@gmail.com>
2016-11-10 14:14:02 +0000

    added signout button, changed to before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :categories
+  before_action :authenticate_user!
+
+  before_action :categories
 
   def categories
     @categories = Category.all
